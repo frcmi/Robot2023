f729c08a8bb1e736ba6fd339acc8e78d432c8dd8
Vittorio <vittorio.latrofa@gmail.com>
2023-03-02 15:59:04 -0800

    Added Vision elements to Robot Container
    Also moved driveSubsystem initialization into constructor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,17 +19,19 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+  public final DriveSubsystem m_driveSubsystem;
   private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
   private final ArmSubsystem m_armSubsystem = new ArmSubsystem();
   private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
+  public final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_PhotonvisionSubsystem = new m_PhotonvisionSubsystem();
+    m_driveSubsystem = new DriveSubsystem(m_PhotonvisionSubsystem);
     // Configure the trigger bindings
     configureBindings();
   }
