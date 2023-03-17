8eea2abf90abf42ae172ce3b875e3f375d1b1452
jruaaa <joeyruan@zoho.com>
2023-03-17 12:53:49 -0700

    Add CommandScheduler to SmartDashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -43,7 +44,9 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+    var commandScheduler = CommandScheduler.getInstance();
+    commandScheduler.run();
+    SmartDashboard.putData(commandScheduler);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
