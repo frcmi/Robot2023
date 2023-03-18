3962abf04385e04df717560eda76b38feabfdff7
Daniel Bereza <blackcatmaxy@gmail.com>
2023-03-18 11:33:59 -0700

    Make elevator PID run constantly in periodic

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,30 +1,20 @@
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
-
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 
-import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.SparkMax;
 import frc.robot.Constants.ElevatorConstants;
-import frc.robot.Constants.OperatorConstants;
 
 
 public class ElevatorSubsystem extends SubsystemBase {
-    //private final CANSparkMax leftMotor = new CANSparkMax(ElevatorConstants.kLeftMotorId, CANSparkMaxLowLevel.MotorType.kBrushless);
-    //private final CANSparkMax rightMotor = new CANSparkMax(ElevatorConstants.kRightMotorId, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final SparkMax leftMotor = new SparkMax(ElevatorConstants.kLeftMotorId, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final SparkMax rightMotor = new SparkMax(ElevatorConstants.kRightMotorId, CANSparkMaxLowLevel.MotorType.kBrushless);
 
@@ -33,6 +23,8 @@ public class ElevatorSubsystem extends SubsystemBase {
         = new ProfiledPIDController(ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD, 
             new TrapezoidProfile.Constraints(ElevatorConstants.kMaxVel, ElevatorConstants.kMaxAccel));
     private final RelativeEncoder encoder = leftMotor.getEncoder();
+
+    private double goalPosition = 0.0;
 
     public ElevatorSubsystem() {
         //leftMotor.restoreFactoryDefaults();
@@ -55,8 +47,9 @@ public class ElevatorSubsystem extends SubsystemBase {
     public void periodic() {
         // SmartDashboard.putData("Elevator PID", pidController);
         // SmartDashboard.putNumber("Elevator Speed", leftMotor.get());
+        setGoalVolts(goalPosition);
+        
         SmartDashboard.putNumber("Elevator Pos", getPosition());
-        setDefaultCommand(run(this::stop));
         Command command = getCurrentCommand();
         if (command != null)
             SmartDashboard.putString("Elevator Command", command.getName());
@@ -71,21 +64,18 @@ public class ElevatorSubsystem extends SubsystemBase {
     private void setVolts(double volts) {
         double position = getPosition();
         boolean outOfBounds = position < ElevatorConstants.minPos || position > ElevatorConstants.maxPos;
-        // Stop movement if outside bounds
+
+        // Clamp movement if outside bounds
         if (position < ElevatorConstants.minPos) 
             volts = Math.max(0, Math.min(2, volts));
         if (position > ElevatorConstants.maxPos)
             volts = Math.max(-2, Math.min(0, volts));
+        
         SmartDashboard.putBoolean("Elevator Bounds", !outOfBounds);
         SmartDashboard.putNumber("Elevator Out Volts", volts);
+
         leftMotor.setVoltage(volts);
         rightMotor.setVoltage(volts);
-    }
-
-    public CommandBase stop() {
-        CommandBase command = run(() -> setVolts(ElevatorConstants.kG));
-        command.setName("ElevatorStop");
-        return command;
     }
 
     public void setGoalVolts(double goalPosition) {
@@ -95,7 +85,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public CommandBase moveTo(double position) {
-        return run(() -> setGoalVolts(position)).until(pidController::atGoal);
+        return run(() -> goalPosition = position).until(pidController::atGoal);
     }
 
     public CommandBase raise() {
